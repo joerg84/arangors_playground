d3c1978aa44b384dcd6e193ccc57b6ad58d9ef15
joerg84 <joerg.schad@gmail.com>
2020-12-28 14:50:58 +0100

    Draft blocking example.

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use anyhow::Error;
+
+use arangors::Connection;
+
+// exact same API except that you don't need to await
+fn main() -> Result<(), Error> {
+    let conn = Connection::establish_jwt("http://localhost:8529", "root", "test123")?;
+    let database = conn.db("test_db")?;
+
+    let collections = database.accessible_collections()?;
+    println!("{:?}", collections);
+
+    let collections = database.accessible_collections()?;
+    println!("{:?}", collections);
+
+    let info = database.info()?;
+    println!("{:?}", info);
+
+    Ok(())
+}
