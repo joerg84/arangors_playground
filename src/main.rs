4b613dc19011b62ef0a0bdcb9160900b5b1d9750
joerg84 <joerg.schad@gmail.com>
2020-12-29 09:49:02 +0100

    Adapted features.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::Error;
-
 use arangors::Connection;
 
 // exact same API except that you don't need to await
 fn main() -> Result<(), Error> {
-    let conn = Connection::establish_jwt("http://localhost:8529", "root", "test123")?;
+    let conn = Connection::establish_jwt("http://127.0.0.1:8529", "root", "test123")?;
     let database = conn.db("test_db")?;
 
     let collections = database.accessible_collections()?;
